2b8453d5f568d29789245b29d872f0c8b77c79bf
LegendaryGameMasters <157835439+LegendaryGameMasters@users.noreply.github.com>
2024-01-27 12:33:33 +0100

    VistaPersonaje

---
+++ Assets/vistapersonaje.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vistaPersonaje : MonoBehaviour
+{
+    public float mouseSensivity = 80f;
+
+    public Transform playerBody;
+
+    float xRotation = 0;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -60f, 50f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, -90, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
+
+
