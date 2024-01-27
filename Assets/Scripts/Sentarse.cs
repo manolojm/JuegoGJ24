076b07d7314b4ac7ab8b454be01d22dabccbeddd
manolojm <manolojm96@gmail.com>
2024-01-27 13:44:14 +0100

    Escenarios

--- Assets/Scripts/Sentarse.cs
+++ Assets/Scripts/Sentarse.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Sentarse : MonoBehaviour
 {
+    public GameObject[] enemigos;
+    public Animator miTelon;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CrearEnemigo();
+        //OpenTelon();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CrearEnemigo() {
+        Debug.Log("Nuevo enemigo");
+        Vector3 posicionEnemigo = new Vector3(0, 1, 3);
+        var enemigoGenerado = enemigos[Random.Range(0, enemigos.Length)];
+        var nuevoEnem = Instantiate(enemigoGenerado, posicionEnemigo, Quaternion.identity);
+        Destroy(nuevoEnem, 2);
+        //CloseTelon();
+
+        Invoke("CrearEnemigo", 3.0f);
+    }
+
+    public void OpenTelon() {
+        miTelon.Play("TelonAbrir");
+    }
+
+    public void CloseTelon() {
+        miTelon.Play("TelonCerrar");
     }
 }
